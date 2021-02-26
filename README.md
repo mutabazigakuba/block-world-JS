ae99043f75597d3c278ac9bcdfb053c247b64370
mutabazigakuba <gakubar2@gmail.com>
2021-02-26 09:32:47 +0200

    did some factorization

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # block-world-JS
 
 ### Usage
-  - clone the project
-  - run npm install
-  - then run npm start
-  - use the cmd to enter input required 
+  - create a folder locally to clone the project into
+  - clone the project `git clone github-link`
+  - run `npm install`
+  - then `run npm start`
+  - use the cmd to enter inputs required 
 
 ### Description
-  - NB: assumes that a is always smaller than b (a is in a position below b)
+  NB: 
+    - assumes that a is always smaller than b (a is in a position below b)
+    - assumes a and b are at the bottom of the stacks (if any)
 
   #### moves
     - move a onto b
--- index.js
+++ index.js
@@ -1,91 +1,85 @@
-import {getUserInput} from './helpers/getUserinput';
-import {moveAoverB} from './helpers/moveAoverB';
-import {moveAontoB} from './helpers/moveAontoB';
-import {pileAontoB} from './helpers/pileAontoB';
-import {pileOverB} from './helpers/pileAoverB';
+import { getUserInput } from './helpers/getUserinput';
+import { moveAoverB } from './helpers/moveAoverB';
+import { moveAontoB } from './helpers/moveAontoB';
+import { pileAontoB } from './helpers/pileAontoB';
+import { pileOverB } from './helpers/pileAoverB';
 
 const prompt = require('prompt-sync')();
 
-var inputNumber  = getUserInput();
-if (Number.isInteger(Number(inputNumber)))
-{
-    var initialBlockWorld = createBlocks(inputNumber)
+start();
 
-    var a = getBlockNumber('A', initialBlockWorld);
-    var b = getBlockNumber('B', initialBlockWorld);
+function start() {
+    var inputNumber = getUserInput();
+    if (Number.isInteger(Number(inputNumber))) {
+        var initialBlockWorld = createBlocks(inputNumber)
 
-    
-    if (!(Number.isInteger(Number(a))))
-    {
-        console.log(a+'\n')
-    }else if (!(Number.isInteger(Number(b))))
-    {
-        console.log(b+'\n')
-    }else if (a == b) 
-    {
-        console.log('A should not be equal to B\n')
-    }else 
-    {
-        var selectedMove = moveOption();
-        if (selectedMove == 1){
-            moveAontoB(a, b, initialBlockWorld);
-        }
-        else if (selectedMove == 2){
-            moveAoverB(a, b, initialBlockWorld);
-        }
-        else if (selectedMove == 3){
-            pileAontoB(a, b, initialBlockWorld);
-        }
-        else if (selectedMove ==4){
-            pileOverB(a, b, initialBlockWorld);
-        }
-        else{
-            console.log('Value not allowed')
+        var a = getBlockNumber('A', initialBlockWorld);
+        var b = getBlockNumber('B', initialBlockWorld);
+
+
+        if (!(Number.isInteger(Number(a)))) {
+            console.log(a + '\n')
+        } else if (!(Number.isInteger(Number(b)))) {
+            console.log(b + '\n')
+        } else if (a == b) {
+            console.log('A should not be equal to B\n')
+        } else {
+            var selectedMove = moveOption();
+            if (selectedMove == 1) {
+                moveAontoB(a, b, initialBlockWorld);
+            }
+            else if (selectedMove == 2) {
+                moveAoverB(a, b, initialBlockWorld);
+            }
+            else if (selectedMove == 3) {
+                pileAontoB(a, b, initialBlockWorld);
+            }
+            else if (selectedMove == 4) {
+                pileOverB(a, b, initialBlockWorld);
+            }
+            else {
+                console.log('Value not allowed')
+            }
         }
     }
-}
-else{
-    console.log(inputNumber)
+    else {
+        console.log(inputNumber)
+    }
 }
 
-function createBlocks (inputNumber) {
+function createBlocks(inputNumber) {
     var initialBlockWorld = new Array(Number(inputNumber))
     var blocks = 0;
-    for (let i=0; i< initialBlockWorld.length; i++)
-    {
+    for (let i = 0; i < initialBlockWorld.length; i++) {
         initialBlockWorld[i] = blocks;
         blocks++
     }
-    console.log('Block numbers created: ['+initialBlockWorld+'] \n')
+    console.log('Block numbers created: [' + initialBlockWorld + '] \n')
     return initialBlockWorld;
 }
 
-function getBlockNumber(number, blockNumbers){
-    var userInput = prompt('Input block number: '+number+' => ' );
-    if(Number.isInteger(Number(userInput)))
-    {
-        for(let i=0; i < blockNumbers.length; i++)
-        {
-            if(Number(userInput) == blockNumbers[i])
-            {
+function getBlockNumber(number, blockNumbers) {
+    var userInput = prompt('Input block number: ' + number + ' => ');
+    if (Number.isInteger(Number(userInput))) {
+        for (let i = 0; i < blockNumbers.length; i++) {
+            if (Number(userInput) == blockNumbers[i]) {
                 return userInput
             }
         }
-        return '\nNumber '+number+' is out of bounds\n';
+        return '\nNumber ' + number + ' is out of bounds\n';
     }
-    else{
-        return '\n'+number+' should be a number\n';
+    else {
+        return '\n' + number + ' should be a number\n';
     }
 }
 
-function moveOption(){
+function moveOption() {
     console.log('')
     const inputOptions = 'Select: #1. Move A onto B #2. Move A over B #3. Pile A onto B #4. Pile A over B => ';
     var selectOption = prompt(inputOptions);
-    if (Number.isInteger(Number(selectOption)))
-    {
+    if (Number.isInteger(Number(selectOption))) {
         return selectOption
-    }else{
+    } else {
         return '\nshould be 1, 2, 3, 4 \n';
     }
 }
